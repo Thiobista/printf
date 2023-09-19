4bd05659e5f0099b9f8a2d07eaedb8601e1c6767
thiobista <thiobista0gedefaw@gmail.com>
2023-09-19 11:02:59 -0700

    utils.c file - 0x11. C - printf -  file created by me Thiobista Gedefaw and my partner Bereket Dereje Mekkonen

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
 * is_printable - Evaluates if a char is printable
-* by Bereket Dereje and Thiobista Gedefaw
+* by Thiobista Gedefaw and Bereket Dereje
 * @c: Char to be evaluated.
 *
 * Return: 1 if c is printable, 0 otherwise
