a321380f1ca7e604d73438430a968e2cb1b1bf0b
thiobista <thiobista0gedefaw@gmail.com>
2023-09-19 11:18:08 -0700

    size.c - 0x11. C - printf -  file created by me Thiobista Gedefaw and my partner Bereket Dereje Mekkonen

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
 * get_size - Calculates the size to cast the argument
-* by Bereket Dereje and Thiobista Gedefaw
+* by Thiobista Gedefaw and Bereket Dereje
 * @format: Formatted string in which to print the arguments.
 * @i: List of arguments to be printed.
 *
