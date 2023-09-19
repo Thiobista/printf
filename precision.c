20db1a041d2042a7b0591b5ed41bf043a5107515
thiobista <thiobista0gedefaw@gmail.com>
2023-09-19 11:09:21 -0700

    precision.c file - 0x11. C - printf -  file created by me Thiobista Gedefaw and my partner Bereket Dereje Mekkonen

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
 * get_precision - Calculates the precision for printing
-* by Bereket Dereje and Thiobista Gedefaw
+* by Thiobista Gedefaw and Bereket Dereje
 * @format: Formatted string in which to print the arguments
 * @i: List of arguments to be printed.
 * @list: list of arguments.
