6b216f862148c08701075101b761a91da569e89b
thiobista <thiobista0gedefaw@gmail.com>
2023-09-19 10:36:47 -0700

    handlers.c- 0x11. C - printf created by me Thiobista Gedefaw and my partner Bereket Dereje Mekkonen

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
 * handle_write_char - Prints a string
-* by Bereket Dereje and Thiobista Gedefaw
+* by Thiobista Gedefaw and Bereket Dereje
 * @c: char types.
 * @buffer: Buffer array to handle print
 * @flags: Calculates active flags.
